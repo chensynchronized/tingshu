a0ad4e4c099eea4d20c55661b75df3179fb5f80a
chm <502015160@qq.com>
2026-08-08 11:39:04 +0800

    提交订单-选择专辑

--- service/service-order/src/main/java/com/atguigu/tingshu/order/service/impl/OrderInfoServiceImpl.java
+++ service/service-order/src/main/java/com/atguigu/tingshu/order/service/impl/OrderInfoServiceImpl.java
@@ -1,10 +1,14 @@
 package com.atguigu.tingshu.order.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.Assert;
 import cn.hutool.core.util.IdUtil;
+import com.atguigu.tingshu.album.AlbumFeignClient;
 import com.atguigu.tingshu.common.constant.RedisConstant;
 import com.atguigu.tingshu.common.constant.SystemConstant;
+import com.atguigu.tingshu.common.execption.GuiguException;
+import com.atguigu.tingshu.model.album.AlbumInfo;
 import com.atguigu.tingshu.model.order.OrderInfo;
 import com.atguigu.tingshu.model.user.VipServiceConfig;
 import com.atguigu.tingshu.order.helper.SignHelper;
@@ -15,6 +19,7 @@ import com.atguigu.tingshu.vo.order.OrderDerateVo;
 import com.atguigu.tingshu.vo.order.OrderDetailVo;
 import com.atguigu.tingshu.vo.order.OrderInfoVo;
 import com.atguigu.tingshu.vo.order.TradeVo;
+import com.atguigu.tingshu.vo.user.UserInfoVo;
 import com.baomidou.mybatisplus.core.toolkit.BeanUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import jakarta.annotation.Resource;
@@ -24,7 +29,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -39,6 +46,8 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
     private UserFeignClient userFeignClient;
     @Resource
     private RedisTemplate redisTemplate;
+    @Resource
+    private AlbumFeignClient albumFeignClient;
 
     /**
      * 处理订单结算页面数据汇总（VIP会员、专辑、声音）
@@ -85,6 +94,55 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoMapper, OrderInfo
             orderDerateVoList.add(orderDerateVo);
         }else if (SystemConstant.ORDER_ITEM_TYPE_ALBUM.equals(tradeVo.getItemType())){
             //3.处理订单选择页数据-专辑
+            //3.1.远程调用用户服务，判读是否已购买
+            Boolean isPaidAlbum = userFeignClient.isPaidAlbum( tradeVo.getItemId()).getData();
+            if (isPaidAlbum){
+                throw new GuiguException(400,"当前用户已购买该专辑");
+            }
+            //3.2.远程调用专辑服务，查询专辑信息
+            AlbumInfo albumInfo = albumFeignClient.getAlbumInfo(tradeVo.getItemId()).getData();
+            Assert.notNull(albumInfo, "专辑信息不存在");
+            //3.3.远程调用用户服务，查询用户信息
+            UserInfoVo userInfoVo = userFeignClient.getUserInfoVoById(userId).getData();
+            Assert.notNull(userInfoVo, "用户信息不存在");
+            //3.4.计算专辑价格
+            originalAmount = albumInfo.getPrice();
+            //3.4.1.用户为vip用户
+            if (userInfoVo.getIsVip() == 1 && userInfoVo.getVipExpireTime().before(new Date())){
+                BigDecimal vipDiscount = albumInfo.getVipDiscount();
+                if (vipDiscount.compareTo(new BigDecimal("-1")) == 0){
+                    orderAmount = originalAmount;
+                }else {
+                    orderAmount = originalAmount.multiply(vipDiscount).divide(new BigDecimal("10"),2, RoundingMode.HALF_UP);
+                    derateAmount = originalAmount.subtract(orderAmount);
+                }
+            }else {
+                //3.4.2.用户为普通用户
+                //普通用户没有折扣
+                BigDecimal discount = albumInfo.getDiscount();
+                if (discount.compareTo(new BigDecimal("-1")) == 0){
+                    orderAmount = originalAmount;
+                }else {
+                    //普通用户有折扣
+                    orderAmount = originalAmount.multiply(discount).divide(new BigDecimal("10"),2, RoundingMode.HALF_UP);
+                    derateAmount = originalAmount.subtract(orderAmount);
+                }
+            }
+            //3.4.添加订单明细
+            OrderDetailVo orderDetailVo = new OrderDetailVo();
+            orderDetailVo.setItemId(tradeVo.getItemId());
+            orderDetailVo.setItemName(albumInfo.getAlbumTitle());
+            orderDetailVo.setItemUrl(albumInfo.getCoverUrl());
+            orderDetailVo.setItemPrice(albumInfo.getPrice());
+            orderDetailVoList.add(orderDetailVo);
+            //3.5.添加订单减免明细
+            if (derateAmount.compareTo(new BigDecimal("0.0")) != 0){
+                OrderDerateVo orderDerateVo = new OrderDerateVo();
+                orderDerateVo.setDerateType(SystemConstant.ORDER_DERATE_ALBUM_DISCOUNT);
+                orderDerateVo.setDerateAmount(derateAmount);
+                orderDerateVo.setRemarks("购买专辑优惠：" + derateAmount);
+                orderDerateVoList.add(orderDerateVo);
+            }
         }else if (SystemConstant.ORDER_ITEM_TYPE_TRACK.equals(tradeVo.getItemType())){
             //4.处理订单选择页数据-声音
         }
